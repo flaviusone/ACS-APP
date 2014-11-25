97af7f16383545c11a9a68667dbfd276fbf2fbec
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-11-25 22:51:15 +0200

    [Proiect] Adaugat varianta bruteforce. Update makefile si script

--- Muf/test_serial.sh
+++ Muf/test_serial.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
-SERIAL_OUTPUT="build/boyer_horspool"
+SERIAL_OUTPUT_H="build/boyer_horspool"
+SERIAL_OUTPUT_BF="build/bruteforce"
 
-echo "Building file..."
-make build-serial
-echo "Running..."
+echo "Building files..."
+make build-serial-BF
+make build-serial-H
+echo "Running tests..."
 
-#Run test 1
-# time ./$SERIAL_OUTPUT inputs/test1
-#Run test 2
-time ./$SERIAL_OUTPUT inputs/test2
-#Run test 3
-# time ./$SERIAL_OUTPUT inputs/test3
+#Run test 1 - Horspool
+echo -e "\nRunning test 1 Horspool"
+time ./$SERIAL_OUTPUT_H inputs/test1
 
+#Run test 1 - Bruteforce
+echo -e "\nRunning test 1 Bruteforce"
+time ./$SERIAL_OUTPUT_H inputs/test1
+
+#Run test 2 - Horspool
+echo -e "\nRunning test 2 Horspool"
+time ./$SERIAL_OUTPUT_H inputs/test2
+
+#Run test 2 - Bruteforce
+echo -e "\nRunning test 2 Bruteforce"
+time ./$SERIAL_OUTPUT_H inputs/test2
+
+#Run test 3 - Horspool
+echo -e "\nRunning test 3 Horspool"
+time ./$SERIAL_OUTPUT_H inputs/test3
+
+#Run test 3 - Bruteforce
+echo -e "\nRunning test 3 Bruteforce"
+time ./$SERIAL_OUTPUT_H inputs/test3
 
 echo "Done"
 make clean
