abc3fa90cbbcb6b7452985e6da033e59c9834b12
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-12-04 18:53:15 +0200

    [Proiect] Modificat modalitate calcul timp. Mutat functie boyer in fisier .h separat

--- Muf/src/boyer_horspool_omp.c
+++ Muf/src/boyer_horspool_omp.c
@@ -5,72 +5,12 @@
 #include <sys/time.h>
 #include <time.h>
 #include <omp.h>
+#include "boyer_horspool.h"
 
 #define CHUNKSIZE 102400
 #define SIZE CHUNKSIZE+200
 
-/* Returns a pointer to the first occurrence of "needle"
- * within "haystack", or NULL if not found. Works like
- * memmem().
- */
 
-/* Note: In this example needle is a C string. The ending
- * 0x00 will be cut off, so you could call this example with
- * boyermoore_horspool_memmem(haystack, hlen, "abc", sizeof("abc")-1)
- */
-const unsigned char *
-boyermoore_horspool_memmem(const unsigned char* haystack, size_t hlen,
-                           const unsigned char* needle,   size_t nlen)
-{
-    size_t scan = 0;
-    size_t bad_char_skip[UCHAR_MAX + 1]; /* Officially called:
-                                          * bad character shift */
-
-    /* Sanity checks on the parameters */
-    if (nlen <= 0 || !haystack || !needle)
-        return NULL;
-
-    /* ---- Preprocess ---- */
-    /* Initialize the table to default value */
-    /* When a character is encountered that does not occur
-     * in the needle, we can safely skip ahead for the whole
-     * length of the needle.
-     */
-    for (scan = 0; scan <= UCHAR_MAX; scan = scan + 1)
-        bad_char_skip[scan] = nlen;
-
-    /* C arrays have the first byte at [0], therefore:
-     * [nlen - 1] is the last byte of the array. */
-    size_t last = nlen - 1;
-
-    /* Then populate it with the analysis of the needle */
-    for (scan = 0; scan < last; scan = scan + 1)
-        bad_char_skip[needle[scan]] = last - scan;
-
-    /* ---- Do the matching ---- */
-
-    /* Search the haystack, while the needle can still be within it. */
-    while (hlen >= nlen)
-    {
-        /* scan from the end of the needle */
-        for (scan = last; haystack[scan] == needle[scan]; scan = scan - 1)
-            if (scan == 0) /* If the first byte matches, we've found it. */
-                return haystack;
-        /* otherwise, we need to skip some bytes and start again.
-           Note that here we are getting the skip value based on the last byte
-           of needle, no matter where we didn't match. So if needle is: "abcd"
-           then we are skipping based on 'd' and that value will be 4, and
-           for "abcdd" we again skip on 'd' but the value will be only 1.
-           The alternative of pretending that the mismatched character was
-           the last character is slower in the normal case (E.g. finding
-           "abcd" in "...azcd..." gives 4 by using 'd' but only
-           4-2==2 using 'z'. */
-        hlen     -= bad_char_skip[haystack[scan]];
-        haystack += bad_char_skip[haystack[scan]];
-    }
-
-    return NULL;
-}
 
 int first (unsigned char * haystack, int chunk_size) {
   int delay = 0;
@@ -135,7 +75,7 @@ int main(int argc, char *argv[]){
   /* Setam num threads */
   omp_set_num_threads(4);
 
-  #pragma omp parallel for private(j, haystack, hay, thread_buff, buffer)
+  #pragma omp parallel for private(j, haystack, hay, thread_buff, buffer, t) shared(timp_total)
   for (j = 0; j < chunks; j++) {
 
     /* Alocam bufferul privat thread_buff */
@@ -167,7 +107,7 @@ int main(int argc, char *argv[]){
     int size = final - delay;
     if (size > 0)
     {
-      gettimeofday(&start,0);
+      double start_time = omp_get_wtime();
 
       long i = 0;
 
@@ -213,12 +153,11 @@ int main(int argc, char *argv[]){
       /* Adaugam rezultatul la buffer */
       results[j] = thread_buff;
 
-      gettimeofday(&finish,0);
+      double stop_time = omp_get_wtime();
 
-      /* Calcul timp desfasurare */
-      t= (finish.tv_sec - start.tv_sec) + (double)(finish.tv_usec - start.tv_usec)
-       / 1000000.0;
-       timp_total += t;
+      #pragma omp atomic
+      timp_total += stop_time - start_time;
+
     }
     free(haystack);
   }
@@ -229,6 +168,6 @@ int main(int argc, char *argv[]){
 
   fclose(fp);
   fclose(fpo);
-  printf("Time elapsed %lf\n", timp_total);
+  printf("Time elapsed %lf\n", timp_total/4);
   return 0;
 }
