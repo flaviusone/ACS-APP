0cfe9dc65490683fab95e74a94d1d84b212ef35e
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-12-17 01:28:03 +0200

    [Proiect] Scis rm *.sh.*

--- Muf/scripts/experiment1/submit_all.sh
+++ Muf/scripts/experiment1/submit_all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Running experiment 1 - Acelasi fisier (1/10GB) variand nr de th si procese"
-rm *.sh.*
 qsub -cwd -q ibm-nehalem.q 10GB_MPI_var_threads.sh
 qsub -cwd -q ibm-nehalem.q 10GB_OMP_var_threads.sh
 qsub -cwd -q ibm-nehalem.q 10GB_PT_var_threads.sh
