fb042f1bf9c602e7da6f081152ef59db13e370b8
flavius.tirnacop <flavius.tirnacop@fep-62-1.grid.pub.ro>
2014-12-20 21:02:11 +0200

    [Proiect] Rezultate teste

--- Muf/scripts/experiment1/submit_all.sh
+++ Muf/scripts/experiment1/submit_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Running experiment 1 - Acelasi fisier (1/10GB) variand nr de th si procese"
-qsub -cwd -q ibm-nehalem.q GB10_MPI_var_threads.sh
-qsub -cwd -q ibm-nehalem.q GB10_OMP_var_threads.sh
-qsub -cwd -q ibm-nehalem.q GB10_PT_var_threads.sh
-qsub -cwd -q ibm-nehalem.q GB1_MPI_var_threads.sh
-qsub -cwd -q ibm-nehalem.q GB1_OMP_var_threads.sh
-qsub -cwd -q ibm-nehalem.q GB1_PT_var_threads.sh
+qsub -cwd -q ibm-opteron.q GB10_MPI_var_threads.sh
+qsub -cwd -q ibm-opteron.q GB10_OMP_var_threads.sh
+qsub -cwd -q ibm-opteron.q GB10_PT_var_threads.sh
+qsub -cwd -q ibm-opteron.q GB1_MPI_var_threads.sh
+qsub -cwd -q ibm-opteron.q GB1_OMP_var_threads.sh
+qsub -cwd -q ibm-opteron.q GB1_PT_var_threads.sh
--- Muf/scripts/experiment2/submit_all.sh
+++ Muf/scripts/experiment2/submit_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running experiment 2 - Fisiere diferite cu acc nr de threaduri si procese"
-qsub -cwd -q ibm-nehalem.q all_files_best_ALL.sh
-qsub -cwd -q ibm-nehalem.q all_files_best_MPI.sh
-qsub -cwd -q ibm-nehalem.q all_files_best_OMP.sh
-qsub -cwd -q ibm-nehalem.q all_files_best_PT.sh
+qsub -cwd -q ibm-opteron.q all_files_best_ALL.sh
+qsub -cwd -q ibm-opteron.q all_files_best_MPI.sh
+qsub -cwd -q ibm-opteron.q all_files_best_OMP.sh
+qsub -cwd -q ibm-opteron.q all_files_best_PT.sh
