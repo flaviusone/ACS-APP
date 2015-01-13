5a7e39acc4732a00161b97b8478feb46802ead3c
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2015-01-13 18:21:49 +0200

    [Proiect] Modificat mpi scripts

--- Muf/teste_prezentare/slide5/10GB/mpi_quad.sh
+++ Muf/teste_prezentare/slide5/10GB/mpi_quad.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+module load libraries/openmpi-1.6-gcc-4.6.3
 MPI_OUTPUT_H="../../../build/boyer_horspool_mpi"
 mpirun -np 1  ./$MPI_OUTPUT_H ../../../inputs/test4 >> quad/mpi
 mpirun -np 2  ./$MPI_OUTPUT_H ../../../inputs/test4 >> quad/mpi
